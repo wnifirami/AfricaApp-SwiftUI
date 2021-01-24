8264c9fea6eb197b36da1c6cdb1476f3e4407860
rami.ounifi <rami.ounifi@yellow-it.fr>
2021-01-24 18:00:43 +0100

    init project

---
+++ AfricaApp-SwiftUI/AfricaApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp_SwiftUIApp.swift
+//  AfricaApp-SwiftUI
+//
+//  Created by Rami Ounifi on 24/1/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
